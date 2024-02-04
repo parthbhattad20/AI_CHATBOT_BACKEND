0b6012a0f6dd0f0132ff0e5df9862eefd9156ce5
Parth Bhattad <bhattadparth20@gmail.com>
2024-02-04 11:17:42 +0530

    first commit

---
+++ dist/db/connection.js
@@ -0,0 +1,21 @@
+import { connect, disconnect } from "mongoose";
+async function connectToDatabase() {
+    try {
+        await connect("mongodb://localhost:27017/mernChat");
+    }
+    catch (error) {
+        console.log(error);
+        throw new Error("Could not Connect To MongoDB");
+    }
+}
+async function disconnectFromDatabase() {
+    try {
+        await disconnect();
+    }
+    catch (error) {
+        console.log(error);
+        throw new Error("Could not Disconnect From MongoDB");
+    }
+}
+export { connectToDatabase, disconnectFromDatabase };
+//# sourceMappingURL=connection.js.map
